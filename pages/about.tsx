8b8199405923211ffa7e5aa8bae3a45fd27972fa
jcalarcon98 <jeancalarcon98@gmail.com>
2021-04-29 23:48:53 -0500

    About me page finished

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,40 +20,44 @@ const AboutPage = () => {
   }, []);
 
   return (
-    <div className='p-4 xl:p-40 overflow-y-auto' ref={scrolledDiv} onScroll={() => handleScroll(scrolledDiv.current.scrollTop)}>
-      <div className='flex flex-col mb-10 text-sm text-white lg:mb-0'>
-        <div className='flex flex-row items-center mb-4'>
-          <p className='flex font-custom text-lg font-semibold'>
-            {t('about')}
-          </p>
-          <span className='ml-2 w-2/5 h-custom bg-blue-500' />
-        </div>
-        <div className='flex justify-center w-full mb-4'>
-          <Image className='rounded-xl bg-blue-500' src='/profile.png' width={150} height={150} />
-        </div>
-        <div className='flex flex-col px-4 font-custom'>
-          <p className='text-sm font-medium leading-loose '>
-            Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
-            Fast-forward to today, and I&apos;ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
-            Here are a few technologies I&apos;ve been working with recently:
-          </p>
+    <div className='scrollbar overflow-y-auto h-full xl:flex xl:justify-center xl:items-center xl:px-14' ref={scrolledDiv} onScroll={() => handleScroll(scrolledDiv.current.scrollTop)}>
+      <div className='p-4 xl:h-4/5 2xl:px-20 2xl:w-4/5'>
+        <div className='flex flex-col mb-10 text-sm h-full justify-center lg:mb-0'>
+          <div className='flex flex-row items-center mb-4'>
+            <p className='flex font-custom text-lg font-semibold text-white md:text-2xl'>
+              {t('about')}
+            </p>
+            <span className='ml-2 w-2/5 h-custom bg-blue-500' />
+          </div>
 
-          <p className='text-sm font-medium leading-loose '>
-            Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
-            Fast-forward to today, and I&apos;ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
-            Here are a few technologies I&apos;ve been working with recently:
-          </p>
+          <div className='flex flex-col xl:flex-row-reverse'>
+            <div className='flex justify-center xl:w-2/5'>
+              <div className='flex w-3/5 justify-center items-center lg:w-9/12'>
+                <Image className='rounded-xl bg-blue-500' src='/profile.png' width={230} height={230} />
+              </div>
+            </div>
+            <div className='flex flex-col justify-center text-white mt-2 font-custom xl:w-4/5'>
+              <p className='text-sm font-medium text-justify leading-loose md:text-base'>
+                {
+                  t('first_description')
+                }
+              </p>
+              <br />
+              <p className='text-sm font-medium text-justify leading-loose md:text-base'>
+                {
+                  t('second_description')
+                }
+              </p>
+            </div>
+          </div>
 
-          <p className='text-sm font-medium leading-loose '>
-            Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
-            Fast-forward to today, and I&apos;ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
-            Here are a few technologies I&apos;ve been working with recently:
-          </p>
-          <h3 className='text-blue-500 font-semibold mt-3'>Technologies:</h3>
-          <div className='mt-4 grid grid-cols-3 gap-10 justify-evenly justify-items-center content-evenly items-center'>
-            {
-              technologies.map(({ id, image, name }) => <TechnologieItem key={id} imageName={image} name={name} />)
-            }
+          <div className='text-white'>
+            <h3 className='text-blue-500 font-semibold mt-3 font-custom md:text-xl'>Technologies:</h3>
+            <div className='mt-4 grid grid-cols-3 gap-10 md:grid-cols-4 lg:grid-cols-7 xl:pr-14'>
+              {
+                technologies.map(({ id, image, name }) => <TechnologieItem key={id} imageName={image} name={name} />)
+              }
+            </div>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,18 +10,20 @@ export default function Home() {
   removeCookie('index');
 
   return (
-    <div className='flex flex-col-reverse justify-center h-full items-center p-4 xl:flex-row lg:p-40'>
-      <div className='w-full mt-4 text-white font-custom flex flex-col justify-center sm:w-4/5 lg:w-full xl:w-3/5 lg:mt-0'>
-        <p className='text-base text-white font-bold lg:mb-2 lg:text-lg'>{`${t('greeting')} ${String.fromCodePoint(0x1F44b)}, ${t('my_name_is')}`}</p>
-        <TypeWriter text='Jean Carlos Alarcón' classElements='text-2xl mb-1 lg:text-7xl lg:mb-4 font-bold text-blue-500' />
-        <p className='text-lg lg:text-4xl mb-4 font-extrabold text-gray-400 uppercase'>{t('position')}</p>
-        <p className='w-full text-sm font-medium mb-4 lg:mb-8 lg:w-4/5 lg:text-lg '>
-          { t('description')}
-        </p>
-        <button type='button' className='w-3/5 border-2 h-7 text-sm mb-4 font-semibold rounded-lg border-blue-500 bg-transparent lg:w-2/5 lg:border-2 lg:h-12 xl:w-1/5 hover:bg-blue-500 focus:outline-none'>{t('download')}</button>
-      </div>
-      <div className='flex items-center w-32 justify-center sm:w-48 lg:justify-start  xl:w-1/5'>
-        <Image className='object-fill hover:opacity-90' src='/sit.svg' alt='Profile' width={240} height={280} />
+    <div className='h-full flex'>
+      <div className='flex flex-col-reverse justify-center items-center p-4 lg:p-24 xl:p-40 xl:flex-row'>
+        <div className='w-full mt-4 text-white font-custom flex flex-col justify-center sm:w-4/5 lg:w-full xl:w-3/5 lg:mt-0'>
+          <p className='text-base text-white font-bold lg:mb-2 lg:text-lg'>{`${t('greeting')} ${String.fromCodePoint(0x1F44b)}, ${t('my_name_is')}`}</p>
+          <TypeWriter text='Jean Carlos Alarcón.' classElements='text-2xl mb-1 lg:text-7xl lg:mb-4 font-bold text-blue-500' />
+          <p className='text-lg lg:text-4xl mb-4 font-extrabold text-gray-400 uppercase'>{t('position')}</p>
+          <p className='w-full text-sm font-medium mb-4 lg:mb-8 lg:w-4/5 lg:text-lg '>
+            { t('description')}
+          </p>
+          <button type='button' className='w-3/5 border-2 h-7 text-sm mb-4 font-semibold rounded-lg border-blue-500 bg-transparent lg:w-2/5 lg:border-2 lg:h-12 xl:w-1/5 hover:bg-blue-500 focus:outline-none'>{t('download')}</button>
+        </div>
+        <div className='flex items-center w-32 justify-center sm:w-48 lg:justify-start  xl:w-1/5'>
+          <Image className='object-fill hover:opacity-90' src='/sit.svg' alt='Profile' width={240} height={280} />
+        </div>
       </div>
     </div>
   );
