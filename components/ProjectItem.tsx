6ced4d04dd462a585de6924fa4d492be3dd0d705
jcalarcon98 <jeancalarcon98@gmail.com>
2021-05-05 01:04:18 -0500

    Improve Light and Dark mode style on mobile

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -12,64 +12,59 @@ const ProjectItem = ({
   type,
   repoUrl = '',
   demoUrl = '',
-}) => {
-
-  console.log('Hello');
-
-  return (
-    <div className='group rounded-sm flex flex-col justify-between bg-gray-300 dark:bg-gray-700 w-full h-72 p-4 py-4 xl:transition xl:duration-500 hover:-translate-y-2 hover:transform '>
-      <div className='flex justify-between items-center'>
-        <FiFolder className='text-4xl text-blue-500' />
-        <div className='flex w-1/5 text-xl justify-between'>
-          <a href={repoUrl} className='hover:text-blue-500'>
-            <FiGithub />
-          </a>
-          <a href={demoUrl} className='hover:text-blue-500'>
-            <BiLinkExternal />
-          </a>
-        </div>
-      </div>
-
-      <div className='font-custom'>
-        <h3 className='text-lg font-semibold mb-2 group-hover:text-blue-500'>{title}</h3>
-        <p className='text-sm font-medium'>
-          {description}
-        </p>
-      </div>
-
-      {
-        features && (
-          <div className='font-custom'>
-            <h3 className='text-sm font-semibold text-blue-500'>Relevant Features</h3>
-            <ul className='list-disc list-inside'>
-              {
-                features.map((currentFeature, index) => (
-                  <li className='text-xs' key={index}>{currentFeature}</li>
-                ))
-              }
-            </ul>
-          </div>
-        )
-      }
-
-      <div className='flex flex-row justify-between items-center'>
-        <div className='font-custom'>
-          {
-            stack.map((stackItem, index) => (
-              <span key={index} className='text-xs mr-2'>
-                {stackItem}
-              </span>
-            ))
-          }
-        </div>
-
-        <div className='text-xs font-semibold rounded-lg px-2 bg-blue-500 text-white'>
-          {type}
-        </div>
+}) => (
+  <div className='group rounded-sm flex flex-col justify-between bg-gray-300 dark:bg-gray-700 w-full h-80 p-4 py-4 xl:transition xl:duration-500 hover:-translate-y-2 hover:transform '>
+    <div className='flex justify-between items-center'>
+      <FiFolder className='text-4xl text-blue-500' />
+      <div className='flex w-1/5 text-xl justify-between'>
+        <a href={repoUrl} className='hover:text-blue-500'>
+          <FiGithub />
+        </a>
+        <a href={demoUrl} className='hover:text-blue-500'>
+          <BiLinkExternal />
+        </a>
       </div>
     </div>
-  );
-};
+
+    <div className='font-custom'>
+      <h3 className='text-lg font-semibold mb-2 group-hover:text-blue-500 mt-2'>{title}</h3>
+      <p className='text-sm font-medium'>
+        {description}
+      </p>
+    </div>
+
+    {
+      features && (
+        <div className='font-custom mt-3'>
+          <h3 className='text-sm font-semibold text-blue-500'>Relevant Features</h3>
+          <ul className='list-disc list-inside'>
+            {
+              features.map((currentFeature, index) => (
+                <li className='text-xs' key={index}>{currentFeature}</li>
+              ))
+            }
+          </ul>
+        </div>
+      )
+    }
+
+    <div className='flex flex-row justify-between items-center  mt-4'>
+      <div className='font-custom w-10/12 flex flex-wrap'>
+        {
+          stack.map((stackItem, index) => (
+            <span key={index} className='text-xs mr-2'>
+              {stackItem}
+            </span>
+          ))
+        }
+      </div>
+
+      <div className='text-xs font-semibold rounded-lg px-2 bg-blue-500 text-white'>
+        {type}
+      </div>
+    </div>
+  </div>
+);
 
 ProjectItem.defaultProps = {
   features: [],
